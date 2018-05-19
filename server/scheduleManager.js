3058a29f071a5dd61ce9be0bd8a9808dba69dc78
morpheous <cerbus.collin@gmail.com>
2018-05-19 01:10:34 -0700

    13

--- server/scheduleManager.js
+++ server/scheduleManager.js
@@ -15,19 +15,20 @@ function INITIALIZE() {
   				const current_live = await require( "./utils/twitch.js" ).getLiveUsers();
   				if ( current_live ) {
   					if ( current_live.length > 0 ) {
-						const currently_active = RU.getKey( RC.RESTREAMING.ACTIVE );
+						const currently_active = await RU.getKey( RC.RESTREAMING.ACTIVE );
   						if ( !currently_active ) {
   							console.log( "no currently restreaming" );
-  							if ( current_live.indexOf( PreferredStreamer ) ) {
+  							if ( current_live.indexOf( PreferredStreamer ) !== -1 ) {
   								console.log( "Preferred Streamer is Online and we are not steraming anything !!!" );
 								await require( "./states/restreaming.js" ).startUser( PreferredStreamer );
   							}
   						}
   						else {
-  							if ( !current_live.indexOf( currently_active ) ) {
+  							if ( current_live.indexOf( currently_active ) === -1 ) {
+  								console.log( "The Stream we were restreaming went offline" );
   								await require( "./states/restreaming.js" ).stop();
   							}
-  							else if ( current_live.indexOf( PreferredStreamer ) ) {
+  							else if ( current_live.indexOf( PreferredStreamer ) !== -1 ) {
   								if ( PreferredStreamer !== currently_active ) {
   									console.log( "We were streaming something else , but now preferred streamer is online" );
   									await require( "./states/restreaming.js" ).startUser( PreferredStreamer );
