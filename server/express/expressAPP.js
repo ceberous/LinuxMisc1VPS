b61011ca1b07e7f78b282f479cbfb3f0b6da308b
morpheous <cerbus.collin@gmail.com>
2018-07-29 16:22:50 -0700

    ***

--- server/express/expressAPP.js
+++ server/express/expressAPP.js
@@ -50,9 +50,9 @@ const ckey = require( PersonalFilePath ).ckey;
 const ckey_length = require( PersonalFilePath ).ckey_length;
 app.post( "/twiliovtoken" , function( req , res ) {
 	
-	if ( !req.body.ckey ) { sendJSONResponse( res , 200 , { result: "" } ); return; }
-	if ( req.body.ckey.length !== ckey_length ) { sendJSONResponse( res , 200 , { result: "" } ); return; }
-	if ( req.body.ckey !== ckey ) { sendJSONResponse( res , 200 , { result: "" } ); return; }
+	if ( !req.body.ckey ) { console.log( "No CKEY" ); sendJSONResponse( res , 200 , { result: "" } ); return; }
+	if ( req.body.ckey.length !== ckey_length ) { console.log( "CKEY Length === " + req.body.ckey.length.toString() ); sendJSONResponse( res , 200 , { result: "" } ); return; }
+	if ( req.body.ckey !== ckey ) { console.log( "CKEY Sent === " + req.body.ckey ); console.log( "CKEY ===" + ckey );  sendJSONResponse( res , 200 , { result: "" } ); return; }
 
 	//var identity = randomName();
 	var identity = twilio_creds.groupVideo3.identity;
