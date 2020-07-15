d03a28c0a143af97d4b7ec664614dd900012b474
ceberous <cerbus.collin@gmail.com>
2020-07-15 00:20:23 -0400

    90

--- server/express/expressAPP.js
+++ server/express/expressAPP.js
@@ -141,7 +141,7 @@ app.post( "/twiliocallsanitizerconfrence" , function( req , res ) {
 	try {
 		const response = new twilio.twiml.VoiceResponse();
 		response.say( "Forwarding" );
-		response.conference( personal.twilio_creds.forward_phone_number );
+		response.dial.conference( personal.twilio_creds.forward_phone_number );
 		//console.log( response );
 		res.set('Content-Type', 'text/xml');
 		//response.hangup();
